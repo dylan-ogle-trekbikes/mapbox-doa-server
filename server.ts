ff1b8b5444ebb1fdeaea1865d5c3bcfd5e9f758d
Dylan Ogle <dylan_ogle@trekbikes.com>
2022-12-16 16:10:53 -0500

    Added health check

--- server.ts
+++ server.ts
@@ -6,7 +6,11 @@ const app = express();
 const port = 3000;
 
 app.get("/search", MapboxSearchController.getSearchSuggestions);
+app.get("/health", (_req,res)=>{
+  res.set('Cache-Control','no-cache')
+  res.status(200).json({status:'available'})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
